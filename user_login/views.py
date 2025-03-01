0d9cd44a20c83d0edab64237dc65ed41bfa520fe
Hardik-Mehta-Python <hardik.mehta212005@gmail.com>
2025-03-01 18:00:19 +0530

    indexpage

--- user_login/views.py
+++ user_login/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello")
+    return render(request,"index.html")
+
+def login(request):
+    return HttpResponse("login")
+
+def signup(request):
+    return HttpResponse('signup')
 
